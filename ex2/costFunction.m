7080408c5e71e52dcfe4cb4580eac76fc041fd96
Rafael Waiti G. Umetsu <rafael.umetsu@gmail.com>
2021-07-25 01:01:26 -0300

    Update costFunction.m

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -17,7 +17,7 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-J = (1/m) * ((-y'*log(sigmoid(X*theta))) - ((1-y)'*log(sigmoid(X*theta))));
+J = (1/m) * (-y'*log(sigmoid(X*theta)) -(1-y)'*log(1-sigmoid(X*theta)));
 
 grad = theta = (1/m) * X'*(sigmoid(X*theta) - y);
 
