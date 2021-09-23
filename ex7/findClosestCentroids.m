cdb47d2d5d6d1ca101a5dbba4b79997f5bf9641d
Rafael Waiti G. Umetsu <rafael.umetsu@gmail.com>
2021-09-22 22:25:26 -0300

    Update findClosestCentroids.m

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -20,14 +20,9 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
-
-
+    for i = 1:size(X,1)
+        [_, idx(i)] = min(sum(abs(X(i,:)-centroids).^2, 2));
+    endfor
 % =============================================================
-
 end
 
