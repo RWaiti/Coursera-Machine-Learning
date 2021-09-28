27779bbf344f435da5591e6a514b2870361dcb9c
Rafael Waiti G. Umetsu <rafael.umetsu@gmail.com>
2021-09-28 20:09:12 -0300

    Update cofiCostFunc.m

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -39,22 +39,9 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+J = (X*Theta' - Y).^2;
+J(find(R==0)) = 0;
+J = (1/2) * sum(sum(J));
 % =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
